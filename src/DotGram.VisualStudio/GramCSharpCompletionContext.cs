34084fbb951dbe507d2f9c509cb012521806cb52
Igor Tkachev <igor.tkachev@gmail.com>
2026-08-24 20:07:30 -0400

    Support Roslyn tooling inside CSharp expressions

--- src/DotGram.VisualStudio/GramCSharpCompletionContext.cs
+++ src/DotGram.VisualStudio/GramCSharpCompletionContext.cs
@@ -16,15 +16,21 @@ public static class GramCSharpCompletionContext
 		while (start > 0 && IsNameCharacter(text[start - 1]))
 			start--;
 
-		if (start == 0 || text[start - 1] != '@' ||
-			start < text.Length && text[start] == '(')
+		if (start > 0 && text[start - 1] == '@' &&
+			(start >= text.Length || text[start] != '('))
 		{
-			prefix = "";
-			return false;
+			prefix = text.Substring(start, position - start);
+			return true;
 		}
 
-		prefix = text.Substring(start, position - start);
-		return true;
+		if (TryGetParenthesizedBounds(text, position, out var expressionStart, out _))
+		{
+			prefix = text.Substring(expressionStart, position - expressionStart);
+			return true;
+		}
+
+		prefix = "";
+		return false;
 	}
 
 	public static bool TryGetExpression(
@@ -43,16 +49,19 @@ public static class GramCSharpCompletionContext
 		var at = position;
 		while (at > 0 && IsNameCharacter(text[at - 1]))
 			at--;
-		if (at == 0 || text[at - 1] != '@' || at < text.Length && text[at] == '(')
+		int end;
+		if (at > 0 && text[at - 1] == '@' && (at >= text.Length || text[at] != '('))
+		{
+			end = position;
+			while (end < text.Length && IsNameCharacter(text[end]))
+				end++;
+		}
+		else if (!TryGetParenthesizedBounds(text, position, out at, out end))
 		{
 			expression = "";
 			expressionStart = symbolStart = symbolLength = 0;
 			return false;
 		}
-
-		var end = position;
-		while (end < text.Length && IsNameCharacter(text[end]))
-			end++;
 
 		var wordStart = position;
 		while (wordStart > at && IsIdentifierCharacter(text[wordStart - 1]))
@@ -66,6 +75,54 @@ public static class GramCSharpCompletionContext
 		symbolStart = wordStart;
 		symbolLength = wordEnd - wordStart;
 		return symbolLength > 0;
+	}
+
+	static bool TryGetParenthesizedBounds(
+		string text, int position, out int expressionStart, out int expressionEnd)
+	{
+		var depth = 0;
+		var open = -1;
+		for (var index = position - 1; index >= 0; index--)
+		{
+			if (text[index] == ')')
+				depth++;
+			else if (text[index] == '(')
+			{
+				if (depth > 0)
+					depth--;
+				else if (index > 0 && text[index - 1] == '@')
+				{
+					open = index;
+					break;
+				}
+			}
+		}
+
+		if (open < 0)
+		{
+			expressionStart = expressionEnd = 0;
+			return false;
+		}
+
+		expressionStart = open + 1;
+		expressionEnd = text.Length;
+		depth = 0;
+		for (var index = expressionStart; index < text.Length; index++)
+		{
+			if (text[index] == '(')
+				depth++;
+			else if (text[index] == ')')
+			{
+				if (depth == 0)
+				{
+					expressionEnd = index;
+					break;
+				}
+				depth--;
+			}
+		}
+
+		return position >= expressionStart && position <= expressionEnd;
 	}
 
 	static bool IsNameCharacter(char character) =>
--- tests/DotGram.VisualStudio.Tests/GramCSharpCompletionContextTests.cs
+++ tests/DotGram.VisualStudio.Tests/GramCSharpCompletionContextTests.cs
@@ -11,6 +11,9 @@ public sealed class GramCSharpCompletionContextTests
 	[InlineData("Rule : @dec", "dec")]
 	[InlineData("Rule = x => @decimal.Pa", "decimal.Pa")]
 	[InlineData("Rule : @global::System.String", "global::System.String")]
+	[InlineData("Rule = when @(", "")]
+	[InlineData("Rule = when @(decimal.Pa", "decimal.Pa")]
+	[InlineData("Rule = when @(Call(value, dec", "Call(value, dec")]
 	public void FindsCSharpNamePrefixAfterTransition(string text, string expected)
 	{
 		Assert.True(GramCSharpCompletionContext.TryGetPrefix(text, text.Length, out var prefix));
@@ -20,8 +23,7 @@ public sealed class GramCSharpCompletionContextTests
 	[Theory]
 	[InlineData("Rule = Other")]
 	[InlineData("Rule = when @(value)")]
-	[InlineData("Rule = when @(value.Me")]
-	public void DoesNotTakeOverGrammarOrParenthesizedExpressions(string text)
+	public void DoesNotTakeOverGrammarOrClosedParenthesizedExpressions(string text)
 	{
 		Assert.False(GramCSharpCompletionContext.TryGetPrefix(text, text.Length, out _));
 	}
@@ -35,6 +37,19 @@ public sealed class GramCSharpCompletionContextTests
 		Assert.True(GramCSharpCompletionContext.TryGetExpression(
 			text, position, out var expression, out var expressionStart, out var symbolStart, out var symbolLength));
 		Assert.Equal("decimal.Parse", expression);
+		Assert.Equal(text.IndexOf("decimal", System.StringComparison.Ordinal), expressionStart);
+		Assert.Equal("Parse", text.Substring(symbolStart, symbolLength));
+	}
+
+	[Fact]
+	public void FindsHoveredSymbolInsideParenthesizedExpression()
+	{
+		const string text = "Rule = when @(decimal.Parse(value))";
+		var position = text.IndexOf("Parse", System.StringComparison.Ordinal) + 2;
+
+		Assert.True(GramCSharpCompletionContext.TryGetExpression(
+			text, position, out var expression, out var expressionStart, out var symbolStart, out var symbolLength));
+		Assert.Equal("decimal.Parse(value)", expression);
 		Assert.Equal(text.IndexOf("decimal", System.StringComparison.Ordinal), expressionStart);
 		Assert.Equal("Parse", text.Substring(symbolStart, symbolLength));
 	}
